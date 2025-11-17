3d9643702fce3108f6b37d8fbf5a0200b44f5ab3
Isai Gonzalez08 <isaigonzalez482@gmail.com>
2025-11-16 22:30:31 -0600

    [ADD] LoginPage validation with zod

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import * as z from "zod";
+import { Input } from "@/components/ui/input";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(5, "El correo electrónico debe tener al menos 5 caracteres.")
+    .max(32, "El correo electrónico debe tener al menos 32 caracteres."),
+  password: z
+    .string()
+    .min(20, "La contraseña debe tener al menos 20 caracteres.")
+    .max(100, "La contraseña debe tener al menos 100 caracteres."),
+});
 
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    console.log(data);
+  }
+
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-center gap-4">
       <Image width={50} height={50} src="/aquaQA.svg" alt="Logo" />
@@ -11,7 +40,49 @@ export default function Login() {
       <h1 className="text-primary text-center text-5xl font-bold">Iniciar sesión</h1>
       <h3 className="text-center text-xl font-semibold text-gray-200">Bienvenido, por favor ingrese sus datos</h3>
 
-      <Button variant="primary" size="lg" className="w-72 text-2xl" onClick={() => console.log("login")}>
+      <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)} className="flex w-full max-w-2/4 flex-col gap-4">
+        <FieldGroup>
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-rhf-demo-email">Correo electrónico</FieldLabel>
+                <Input
+                  {...field}
+                  id="form-rhf-demo-email"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Correo electrónico"
+                  autoComplete="off"
+                />
+                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              </Field>
+            )}
+          />
+        </FieldGroup>
+
+        <FieldGroup>
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-rhf-demo-password">Contraseña</FieldLabel>
+                <Input
+                  {...field}
+                  id="form-rhf-demo-password"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Contraseña"
+                  autoComplete="off"
+                />
+                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              </Field>
+            )}
+          />
+        </FieldGroup>
+      </form>
+
+      <Button variant="primary" size="lg" className="mt-4 w-72 text-2xl" type="submit" form="form-rhf-demo">
         Iniciar sesión
       </Button>
     </div>
