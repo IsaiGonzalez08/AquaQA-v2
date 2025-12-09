7cd9ed2bc3d6321eb980a03d9d2e54b1877126bf
Isai Gonzalez08 <isaigonzalez482@gmail.com>
2025-12-09 13:08:54 -0600

    [ADD] expand sidebar icon added

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./components/Sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
@@ -8,7 +8,10 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     <SidebarProvider>
       <div className="flex min-h-screen w-full">
         <AppSidebar />
-        <SidebarInset className="flex-1 bg-gray-50 p-10">{children}</SidebarInset>
+        <SidebarInset className="flex-1 p-10">
+          <SidebarTrigger/>
+          <div>{children}</div>
+        </SidebarInset>
       </div>
     </SidebarProvider>
   );
--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -7,8 +7,8 @@ export default function UserDashboard() {
   const { user } = useSelector((state: RootState) => state.auth);
 
   return (
-    <div className="space-y-6">
-      <h1 className="text-4xl font-bold">Bienvenido, {user?.name} 👋</h1>
+    <div>
+      <h1 className="text-4xl font-bold mt-10">Bienvenido, {user?.name} 👋</h1>
       <p className="mt-2 text-gray-400">Aquí está tu resumen de actividad</p>
     </div>
   );
