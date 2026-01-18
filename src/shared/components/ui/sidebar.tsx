e6f453192fdcc613689223f1abb68e05658c0a84
IsaiGonzalez <isaigonzalez482@gmail.com>
2026-01-18 11:58:07 -0600

    [FIX] button variants removed

--- src/shared/components/ui/sidebar.tsx
+++ src/shared/components/ui/sidebar.tsx
@@ -240,8 +240,6 @@ function SidebarTrigger({ className, onClick, ...props }: React.ComponentProps<t
     <Button
       data-sidebar="trigger"
       data-slot="sidebar-trigger"
-      variant="ghost"
-      size="icon"
       className={cn("size-7", className)}
       onClick={(event) => {
         onClick?.(event);
